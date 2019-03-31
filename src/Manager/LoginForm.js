621eceb6a7136baed08b331f2394c7ceef1572bf
Việt Nguyễn <vietqb9779@gmail.com>
2019-03-31 13:38:55 +0700

    finish section15

--- src/Manager/LoginForm.js
+++ src/Manager/LoginForm.js
@@ -1,11 +1,34 @@
 import React, {Component} from 'react';
+import {View, TextInput} from 'react-native';
+import {connect} from 'react-redux';
+export {emailChanged} from './action';
 import Button from '../components/Button';
 import Card from '../components/Card';
 import CardSection from '../components/CardSection';
-export default class LoginForm extends Component{
+class LoginForm extends Component{
+    
+    onEmailChange(text){
+        this.props.emailChanged(text);
+    }
+
     render(){
         return(
             <Card>
+            <CardSection>
+            <TextInput
+            style={styles.textInputStyle}
+            onChangeText={this.onEmailChange.bind(this)}
+            placeholder='Enter your email'
+            />
+            </CardSection>
+            
+            <CardSection>
+            <TextInput
+            secureTextEntry
+            style={styles.textInputStyle}
+            placeholder='Enter your password'
+            />
+            </CardSection>
             <CardSection>
             <Button>
             Login
@@ -15,3 +38,15 @@ export default class LoginForm extends Component{
         );
     }
 }
+
+const styles = {
+    textInputStyle:{
+        height: 40, 
+        flex:1, 
+        borderColor: 'gray', 
+        borderWidth: 1,
+        borderRadius:20
+    },
+}
+
+export default connect(null,)(LoginForm);
---
+++ src/Manager/reducer/AuthReducer.js
@@ -0,0 +1,10 @@
+import {EMAIL_CHANGED} from '../action/type';
+const INITIAL_STATE = {email:''};
+export default (state= INITIAL_STATE, action)=> {
+    switch (action.type) {
+        case EMAIL_CHANGED:
+        
+        default:
+            return state;
+    }
+}
